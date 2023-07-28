fd60ffe78fcc787ed6b2e6b770ad3f48abcfb94f
gloria <gloria.gao115@gmail.com>
2023-07-28 23:54:55 +1000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,8 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import Navbar from "./NavBar";
 
 {/* write an <About> component here */}
 
@@ -22,9 +10,9 @@ function Home() {
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar/>
+      <Home/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+export default Navbar;
